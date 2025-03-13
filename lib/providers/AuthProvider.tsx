576523096f9eadd0281d2ea9c9ddabeb9c58d0f9
daniel-dunsin <adejaredaniel12@gmail.com>
2025-03-13 04:01:59 +0100

    fix: merge conflict

--- lib/providers/AuthProvider.tsx
+++ lib/providers/AuthProvider.tsx
@@ -4,14 +4,14 @@ import LogoLoader from '@/components/Common/Loaders/logo.loader';
 import { useSession } from 'next-auth/react';
 import React, { FC, ReactNode, useEffect } from 'react';
 import useUserStore from '../store/user.store';
-import { getUser } from '../services/user.service';
+import { getUserInfo } from '../services/user.service';
 
 const AuthProvider: FC<{ children: ReactNode }> = ({ children }) => {
   const { status } = useSession();
   const { user, setUser } = useUserStore();
 
   useEffect(() => {
-    getUser().then((user) => {
+    getUserInfo().then((user) => {
       setUser(user);
       console.log(user);
     });
--- lib/services/user.service.ts
+++ lib/services/user.service.ts
@@ -1,18 +1,5 @@
-<<<<<<< HEAD
-import { ApiResponse } from '../@types';
-import { User } from '../@types/auth';
 import { authApi } from '../configs/axios-instance';
-
-export const getUser = async () => {
-  try {
-    const response = await authApi.get<ApiResponse<User>>('/user');
-
-    return response?.data?.data;
-  } catch (err: any) {
-    throw new Error(err?.response.data.msg || 'Something went wrong');
-=======
-import { authApi } from "../configs/axios-instance";
-import { ApiResponse, User } from "../@types";
+import { ApiResponse, User } from '../@types';
 
 export const getUserInfo = async () => {
   try {
@@ -21,7 +8,6 @@ export const getUserInfo = async () => {
     } = await authApi.get<ApiResponse<User>>(`/user`);
     return data;
   } catch (err: any) {
-    throw new Error(err?.response.data.msg || "Something went wrong");
->>>>>>> 23c1daa9f9507dc1e310253fcbe9c50c35407e1e
+    throw new Error(err?.response.data.msg || 'Something went wrong');
   }
 };
